6822f464a294ccbd8d35d01a8406567958d95375
Daniel Zadok <zadok.daniel@gmail.com>
2022-11-21 11:01:56 +0200

    extract header from home and about into PageHeader component

--- W040422MR - 2022.11.21/real-app-front/changelog.md
+++ W040422MR - 2022.11.21/real-app-front/changelog.md
@@ -1,3 +1,29 @@
+## extract header from home and about into PageHeader component
+
+```jsx
+const PageHeader = ({ title, description }) => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-12 mt-4">
+          <h1>{title}</h1>
+        </div>
+      </div>
+
+      {description ? (
+        <div className="row">
+          <div className="col-12">
+            <p>{description}</p>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default PageHeader;
+```
+
 ## about page component
 
 ```jsx
--- W040422MR - 2022.11.21/real-app-front/src/components/home.jsx
+++ W040422MR - 2022.11.21/real-app-front/src/components/home.jsx
@@ -1,23 +1,15 @@
+import PageHeader from "./common/pageHeader";
+
 const Home = () => {
   return (
-    <>
-      <div className="row">
-        <div className="col-12 mt-4">
-          <h1>
-            Real<i className="bi bi-geo-fill"></i>App
-          </h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
-            neque accusamus pariatur reprehenderit nemo beatae facere obcaecati
-            est impedit numquam.
-          </p>
-        </div>
-      </div>
-    </>
+    <PageHeader
+      title={
+        <>
+          Real<i className="bi bi-geo-fill"></i>App
+        </>
+      }
+      description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam corporis ex doloribus dignissimos hic tempore voluptatem eligendi eaque expedita rem."
+    />
   );
 };
 
