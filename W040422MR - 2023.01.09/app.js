58cf3d874056a4052c9a61ff45bc53398e4c3b61
Daniel Zadok <zadok.daniel@gmail.com>
2023-01-09 16:02:19 +0200

    nodejs start

---
+++ W040422MR - 2023.01.09/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+console.log(express);
+
+console.log("hello");
+
+const str = "dsf";
+
+for (s of str) {
+  console.log(s);
+}
+
+// create new project with add function use the function and console to
+// show how much is 5+10, 16+4
+
+// create new project with multiplaction function use the function and console to
+// show how much is 5*10, 16*4
+
+// create new project with divide function use the function and console to
+// show how much is 5/10, 16/4
